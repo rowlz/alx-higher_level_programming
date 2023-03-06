619ca03c5aa788cc76b55582ce60e433d70887c9
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-06 07:33:37 -0800

    fixed task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * check_cycle - checkas cycle
+ * @list: list
+ *
+ * Return: 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	(void) list;
+
+	return (1);
+}
