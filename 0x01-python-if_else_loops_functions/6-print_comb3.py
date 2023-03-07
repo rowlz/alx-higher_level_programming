35be56d0f285aafaa21d631fe80b7d573ccf26ab
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-07 05:38:11 -0800

    fixed task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         print("{}".format(str(i) + str(j)), end="")
-        if int(str(i) _ str(j)) < 89:
+        if int(str(i) + str(j)) < 89:
             print(", ", end="")
 print("")
