4ea14008106a08fbd3d0b04cb18be1d16e248d00
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-07 06:00:30 -0800

    Fixed task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     tmp = list(str)
     for i in range(len(tmp)):
-        if (ord(tmp[i] > 96 and ord(tmp[i]) < 123):
-                tmp[i] = chr(ord(tmp[i]) - 32)
+        if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
+            tmp[i] = chr(ord(tmp[i]) - 32)
     print("{}".format(("").join(tmp)))
