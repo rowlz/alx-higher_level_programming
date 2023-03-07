3881c5ae3830a1edc978eebbd6ccc3e369f5da08
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-07 08:52:46 -0800

    Added task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * insert_node - node function
+ * @head: head
+ * @nmber: number
+ *
+ * Description: thid
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	(void) head;
+	(void) number;
+
+	return NULL;
+}
