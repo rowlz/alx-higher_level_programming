c44381dc3b55a04046d6db26a5f4e3fe4693ef3d
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-09 04:00:45 -0800

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total = len(sys.argv)
+    a_sum = 0
+    if total <= 1:
+        print("0")
+    else:
+        if total == 2:
+            print("{}".format(sys.argv[1]))
+        else:
+            for i in range(1, total):
+                a_sum += sys.argv[int(i)]
+    print("{}".format(a_sum))
