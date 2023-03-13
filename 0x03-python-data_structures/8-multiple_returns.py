d6daa57bbb1efb26fc93f7903c771a8cce51509d
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-13 06:56:33 -0700

    Added task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_length = len(sentence)
     if sentence[0] is None:
         first_char = None
+        tuple_length = 0
+        return tuple_length, first_char
     else:
         first_char = sentence[0]
-    return tuple_length, first_char
+        tuple_length = len(sentence)
+        return tuple_length, first_char
