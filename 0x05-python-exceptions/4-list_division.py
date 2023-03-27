640d8d5e4977625c584c4cfbea71f1cd556eb030
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-27 07:43:35 -0700

    Fix task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,4 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             result = 0
         finally:
-            new_l
+            new_list.append(result)
+    return new_list
