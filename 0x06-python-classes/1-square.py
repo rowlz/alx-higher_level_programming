d8584fc45d93b13ff9c0cb19133d6910c0a2c91b
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-03-28 06:41:13 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" A square class
+"""
+
+
+class Square:
+    """ An square class that defines a square with size
+    """
+
+    def __init__(self, size):
+        """ The Square class initialization
+        Args:
+            size: size of the square
+        """
+        self.__size = size
