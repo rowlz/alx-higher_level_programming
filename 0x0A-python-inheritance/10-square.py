913ca63fdb3dec7727c31c984e92e355ddd85264
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-04-11 07:19:28 -0700

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square Module
+Attributes:
+    Rectangle (object): Base Object
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    """ Square Class
+    """
+
+    def __init__(self, size: int) -> None:
+        """ instance initialization function
+        Args:
+            size (int): Description
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
