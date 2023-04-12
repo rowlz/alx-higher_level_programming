d8464da0fc8574b60bcbe06d8fe6af4051ea8a2b
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-04-12 06:25:20 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is the read_file module
+"""
+
+
+def read_file(filename=""):
+    """ read_file function reads a file and prints it to stdot
+        Args:
+            filename: file name variable
+        Return:
+            reads the file
+    """
+
+    with open(filename, encoding='utf-8') as a_file:
+        for line in a_file:
+            print(line, end="")
