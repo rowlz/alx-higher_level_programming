73ceb447b7aacd69c34a3ada812665878bab40b8
Rowland Oladiran <rowlandoladiran3@gmail.com>
2023-04-12 07:42:02 -0700

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is the load_from_json_file module
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ load_from_json_file function create an Object from a text file
+        Args:
+            filename: file name variable
+        Return:
+            an object
+    """
+
+    with open(filename, encoding='utf-8') as a_file:
+        return (json.load(a_file))
